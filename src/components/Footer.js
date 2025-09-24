50098fafc320af37b529435196137b90b1db0364
Sergey Radzivillovich <prepod2003@yandex.ru>
2025-09-24 17:22:31 +0300

    feat: Implement responsive desktop grid layout

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,14 +6,11 @@ const Footer = () => {
     <footer id="footer" className="footer">
       <div className="container">
         <h2>Контакты</h2>
+        <p className="footer-name">Радзивиллович Сергей</p>
         <div className="contact-info">
-          <p>📍 Санкт-Петербург</p>
-          <p>
-            <a href="tel:+79045596847">📞 +7 (904) 559-68-47</a>
-          </p>
-          <p>
-            <a href="mailto:prepod2003@yandex.ru">📧 prepod2003@yandex.ru</a>
-          </p>
+          <div className="contact-item"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z" fill="currentColor"/></svg><span>Санкт-Петербург</span></div>
+          <div className="contact-item"><a href="tel:+79045596847"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M6.62 10.79c1.44 2.83 3.76 5.14 6.59 6.59l2.2-2.2c.27-.27.67-.36 1.02-.24 1.12.37 2.33.57 3.57.57.55 0 1 .45 1 1V20c0 .55-.45 1-1 1-9.39 0-17-7.61-17-17 0-.55.45-1 1-1h3.5c.55 0 1 .45 1 1 0 1.25.2 2.45.57 3.57.11.35.03.74-.25 1.02l-2.2 2.2z" fill="currentColor"/></svg><span>+7 (904) 559-68-47</span></a></div>
+          <div className="contact-item"><a href="mailto:prepod2003@yandex.ru"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z" fill="currentColor"/></svg><span>prepod2003@yandex.ru</span></a></div>
         </div>
         <div className="social-links">
           <a href="https://t.me/Prepod2003" target="_blank" rel="noopener noreferrer" className="social-link telegram">Telegram</a>
